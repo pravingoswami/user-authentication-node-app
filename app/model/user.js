1a1d4448a8a3398a69e1483e822014090242f389
pravingoswami <goswami4737@gmail.com>
2020-02-02 11:50:56 +0530

    created model for user

---
+++ app/model/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        unique :true,
+        required : true
+    },
+
+    email : {
+        type : String,
+        unique : true,
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    },
+
+    createdAt : {
+        type : Date,
+        required : Date.now()
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+    User
+}
