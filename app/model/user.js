4bf51a5ab29e78fff113f3d4a26ef6af002c718f
pravingoswami <goswami4737@gmail.com>
2020-02-02 15:09:46 +0530

    custom validations

--- app/model/user.js
+++ app/model/user.js
@@ -1,22 +1,35 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     username : {
         type : String,
         unique :true,
-        required : true
+        required : true,
+        minlength : 5
     },
 
     email : {
         type : String,
         unique : true,
-        required : true
+        required : true,
+        // how to check the format of the email
+        validate : {
+            validator : function(value){
+                return validator.isEmail(value)
+            },
+            message : function(){
+                return 'invalid email format'
+            }
+        }
     },
 
     password : {
         type : String,
-        required : true
+        required : true,
+        minlength : 6,
+        maxlength : 128
     },
 
     createdAt : {
